74f311df773e80615f602ccd27ada3d07d3e4798
Julien Bigot <julien.bigot@cea.fr>
2020-12-18 08:21:24 +0100

    TD2

---
+++ glcs-td2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+if [ 1 -lt $# ]
+then
+	echo "Usage: $0 [<TDDIR>]" >&2
+	exit 1
+elif [ 1 -eq $# ]
+then
+	TDDIR="$1"
+else
+	TDDIR="TD2"
+fi
+
+if [ ! -e "${TDDIR}" ]
+then
+	echo "Creation du répertoire ${TDDIR}"
+	mkdir -p "${TDDIR}"
+fi
+
+if [ ! -d "${TDDIR}" ]
+then
+	echo "${TDDIR} n'est pas un répertoire!" >&2
+	exit 1
+fi
+
+cd "${TDDIR}"
+
+docker run -v "${PWD}:/data" --rm -ti jbigot/glcs_td2
